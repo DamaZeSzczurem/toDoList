e32a8593987a814d6f54dbde06b37cd24b89c1b2
DamaZeSzczurem <czarna.kawe.prosze@gmail.com>
2021-11-16 10:26:31 +0100

    0.2 final update

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import localePl from '@angular/common/locales/pl';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePl);
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer/footer.component';
+import { HeaderComponent } from './footer/header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
